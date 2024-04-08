cadeea1ab09a710792e32001046423ef73c855d1
Yevhenii-Azarov <azarow1999@gmail.com>
2024-04-08 15:46:46 +0200

    goit-js-hw-01

---
+++ js/task-3.js
@@ -0,0 +1,14 @@
+function getElementWidth(content, padding, border) {
+  const contentWidth = Number.parseFloat(content);
+  const paddingHorizontal = Number.parseFloat(padding);
+  const borderThickness = Number.parseFloat(border);
+  const result = contentWidth + (paddingHorizontal + borderThickness) * 2;
+  return result;
+
+}
+
+
+
+console.log(getElementWidth("50px", "8px", "4px")); // 74
+console.log(getElementWidth("60px", "12px", "8.5px")); // 101
+console.log(getElementWidth("200px", "0px", "0px")); // 200
